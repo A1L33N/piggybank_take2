d9a09a79740c0fb6ef71b1c1af7d36945e472caa
Lisa Folliard & Aileen Cacayorin <student@epicodus.com>
2015-09-01 12:19:50 -0700

    create transaction validations

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,9 @@
 class Transaction < ActiveRecord::Base
   belongs_to(:kid)
+
+  validates(:kid_id, {:presence => true })
+  validates(:amount, {:presence => true })
+  validates(:type, {:presence => true})
+  validates(:description, {:presence => true})
+  validates(:date, {:presence => true})
 end
