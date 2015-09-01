1bb46cd39d5cf190bf46f43a58469001b4af94a9
Lisa Folliard & Aileen Cacayorin <student@epicodus.com>
2015-09-01 11:56:30 -0700

    pass parent validations and kids method

--- spec/parent_spec.rb
+++ spec/parent_spec.rb
@@ -5,4 +5,9 @@ describe(Parent) do
     it { should have_many(:kids) }
   end
 
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:bank_name) }
+  it { should validate_length_of(:name) }
+  it { should validate_length_of(:bank_name) }
+
 end
