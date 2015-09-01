cbb661e70c24eda4622002ad2668a05519f629e1
Aileen Cacayorin & Lisa Folliard <student@epicodus.com>
2015-09-01 14:28:17 -0700

    add sum method to transaction class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,7 +3,7 @@ class Transaction < ActiveRecord::Base
 
   validates(:kid_id, {:presence => true })
   validates(:amount, {:presence => true })
-  validates(:type, {:presence => true})
+  validates(:transaction_type, {:presence => true})
   validates(:description, {:presence => true})
   validates(:date, {:presence => true})
 end
