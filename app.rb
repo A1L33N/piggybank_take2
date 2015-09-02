5830203e7586462f4d31a5614fdc08befe2fc484
Guest User <student@epicodus.com>
2015-09-02 10:12:38 -0700

    show index page

--- app.rb
+++ app.rb
@@ -3,3 +3,12 @@ Bundler.require(:default)
 require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+
+get('/') do
+  erb(:index)
+end
+
+get('/parents') do
+  
+end
