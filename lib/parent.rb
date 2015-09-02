f44ccdcdcdf058021e133cbc0c71d498c2ba3d0e
Aileen Cacayorin & Lisa Folliard <student@epicodus.com>
2015-09-02 10:56:52 -0700

    Add chore class, table, and assocations

--- lib/parent.rb
+++ lib/parent.rb
@@ -1,5 +1,6 @@
 class Parent < ActiveRecord::Base
   has_many(:kids)
+  has_many(:chores)
 
   validates(:name, {:presence => true, :length => { :maximum => 50 }})
   validates(:bank_name, {:presence => true, :length => { :maximum => 50 }})
