411d1cfa31a8fc22416177707aa53d1ab7419706
Guest User <student@epicodus.com>
2015-09-02 15:12:13 -0700

    parents pages and kid login

--- app.rb
+++ app.rb
@@ -7,16 +7,19 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 
 get ("/") do
+  @title = ""
   erb(:index)
 end
 
 
 #####Parents
 get('/parents') do
+  @title = "Parent Login"
   @parents = Parent.all
   erb(:parents)
 end
 
+#add new parent
 post('/parents') do
   name = params.fetch('parent_name')
   bank_name = params.fetch('bank_name')
@@ -24,14 +27,16 @@ post('/parents') do
   redirect back
 end
 
+#get one parent
 get('/parents/:id') do
-  id = params.fetch('id').to_i
+  id = params.fetch('id').to_i()
   @parent = Parent.find(id)
   @kids = @parent.kids()
+  @title = @parent.bank_name
   erb(:parent)
 end
 
-
+#post new kid to parent
 post('/parents/:id') do
   id = params.fetch('id').to_i
   @parent = Parent.find(id)
@@ -44,14 +49,36 @@ post('/parents/:id') do
   redirect back
 end
 
-
-######Kids
-get('/kids/:id') do
+#parents - see kid's account
+get('/parent/kids/:id') do
   id = params.fetch('id').to_i
   @kid = Kid.find(id)
   @transactions = @kid.transactions
-  erb(:kid)
+  @title = @kid.account_name
+  @current_date = Time.now.strftime("%Y-%m-%d")
+  erb(:parent_kid)
 end
+
+
+
+
+######Kids
+
+get('/kid') do
+  newname = params.fetch('kid_name')
+  whereclause = "lower(name) = '" + newname.downcase + "'"
+  there = Kid.where(whereclause)
+
+  if there == []
+    @@message = "We can't find your account.  Ask your parents to login"
+    redirect '/'
+  else
+    @@message = ""
+    @kid = there.first
+    redirect '/kid/' + @kid.id.to_s()
+  end
+end
+
 
 post('/kids/:id') do
   id = params.fetch('id').to_i
