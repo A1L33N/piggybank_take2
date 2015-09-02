17d39b9b51ee1cc64e1e7642fde68b55bdbd1f16
Aileen Cacayorin & Lisa Folliard <student@epicodus.com>
2015-09-02 11:00:22 -0700

    Add validations to chore class

--- spec/chore_spec.rb
+++ spec/chore_spec.rb
@@ -1,6 +1,9 @@
 require('spec_helper')
 
 describe(Chore) do
+  it { should validate_presence_of (:description) }
+  it { should validate_presence_of (:pay) }
+
   describe('#parent') do
     it { should belong_to(:parent) }
   end
