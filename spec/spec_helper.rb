7699f5f4b6321262efc532afef192c9bcfc9c83b
Lisa Folliard & Aileen Cacayorin <student@epicodus.com>
2015-09-03 09:44:08 -0700

    add a kid account to parent bank

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,26 @@ Bundler.require(:default, :test)
 set(:root, Dir.pwd())
 
 
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
 require('capybara/rspec')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 require('./app')
 
-
-Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+RSpec.configure do |config|
+  config.after(:each) do
+    Parent.all().each do |parent|
+      parent.destroy()
+    end
+    Kid.all().each do |kid|
+      kid.destroy()
+    end
+    Chore.all().each do |chore|
+      chore.destroy()
+    end
+    Transaction.all().each do |transaction|
+      transaction.destroy()
+    end
+  end
+end
