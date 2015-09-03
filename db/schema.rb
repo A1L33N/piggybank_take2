480661038d4b74b6db64b7c1caea2edfae3e2f70
Lisa Folliard & Aileen Cacayorin <student@epicodus.com>
2015-09-03 14:11:42 -0700

    remove complete column from requests table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903170636) do
+ActiveRecord::Schema.define(version: 20150903210748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 20150903170636) do
     t.string   "description"
     t.integer  "kid_id"
     t.integer  "parent_id"
-    t.boolean  "complete"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
