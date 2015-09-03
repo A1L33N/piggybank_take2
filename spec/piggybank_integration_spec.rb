c993e403752a8f7f41d9cadd903640a83e797175
Lisa Folliard & Aileen Cacayorin <student@epicodus.com>
2015-09-03 09:57:48 -0700

    add a chore

--- spec/piggybank_integration_spec.rb
+++ spec/piggybank_integration_spec.rb
@@ -25,8 +25,22 @@ describe('adding a kid account to a parent bank', {:type => :feature}) do
     fill_in('account_name', :with => 'Ben Dough')
     fill_in('beginning_balance', :with => 34.00)
     fill_in('goal', :with => 150.00)
-
     click_button('Add')
     expect(page).to have_content('Ben')
+  end
+end
+
+describe('adding a chore', {:type => :feature}) do
+  it('add a chore') do
+    visit('/parents')
+    fill_in('parent_name', :with => 'Betty Smith')
+    fill_in('bank_name', :with => 'Betty Bucks')
+    click_button("Add Parent/Bank")
+    click_link("Betty Bucks")
+    click_link('Add chores')
+    fill_in('description', :with => "do laundry")
+    fill_in('pay', :with => 5.00)
+    click_button('Add chore')
+    expect(page).to have_content('do laundry')
   end
 end
